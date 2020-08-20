47bc5df1314eb37ca1b442209a119a4c8dcd3ea0
Nicholas White <nwhite17@huskers.unl.edu>
2020-08-20 17:05:54 +0000

    add hello.c

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+*Author: Nicholas White
+*Date: 2020
+*
+*
+* A simple hello world program
+* */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+
+    printf("Nicholas White\n");
+    printf("Mathematics\n");
+
+    return 0;
+
+}
